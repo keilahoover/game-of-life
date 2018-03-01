6c3ef4f71d779e627c83dd86c125e3377dbd1ce3
Keila Hoover <keila.hoover@gmail.com>
2018-03-01 14:08:48 -0700

    Add disabled functionality

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
 $(document).ready(function () {
+
+  // SET INITIAL UI STATE
   $('#stop-tick-btn').attr('disabled', true)
+
+  $('#dump-config-btn').click(dumpConfigBtnClick)
+  $('#clear-btn').click(clearBtnClick)
+  $('#one-tick-btn').click(oneTickBtnClick)
+  $('#start-tick-btn').click(startTickBtnClick)
+  $('#stop-tick-btn').click(stopTickBtnClick)
+
+  // BUTTON EVENT HANDLERS
+  function stopTickBtnClick(event) {
+
+    $('#dump-config-btn').removeAttr('disabled');
+    $('#clear-btn').removeAttr('disabled');
+    $('#one-tick-btn').removeAttr('disabled');
+    $('#start-tick-btn').removeAttr('disabled');
+
+    $('#stop-tick-btn').attr('disabled', false)
+
+    event.preventDefault()
+    console.log('stopBtnClick()')
+  }
+
+  function oneTickBtnClick(event) {
+    event.preventDefault()
+    console.log('oneTickBtnClick()')
+  }
+
+  function startTickBtnClick(event) {
+    event.preventDefault()
+
+    $('#start-tick-btn').attr('disabled', true)
+    $('#stop-tick-btn').removeAttr('disabled')
+
+    console.log('startTickBtnClick()')
+  }
+
+  function clearBtnClick(event) {
+    event.preventDefault()
+    console.log('clearBtnClick()')
+  }
+
+  function dumpConfigBtnClick(event) {
+    event.preventDefault()
+    console.log('dumpConfigBtnClick()')
+  }
 })
